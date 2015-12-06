19c9fa4145488dd904e1aed04ca662f361a04620
zakharovas <alexkru2011@gmail.com>
2015-12-06 13:23:52 +0300

    Queue tested

--- zakharovas/src/test/java/ru/fizteh/fivt/students/zakharovas/threads/blockingqueue/MyBlockingQueueTest.java
+++ zakharovas/src/test/java/ru/fizteh/fivt/students/zakharovas/threads/blockingqueue/MyBlockingQueueTest.java
@@ -5,9 +5,15 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by alexander on 06.12.2015.
@@ -24,11 +30,8 @@ public class MyBlockingQueueTest {
     @Test
     public void testBlockingOnEmpty() throws Exception {
         MyBlockingQueue<Integer> queue = new MyBlockingQueue<>(5);
-        Thread testingThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                queue.take(5);
-            }
+        Thread testingThread = new Thread(() -> {
+            queue.take(5);
         });
         testingThread.start();
         Thread.sleep(50);
@@ -40,11 +43,8 @@ public class MyBlockingQueueTest {
     public void testBlockingOnOverFlowing() throws Exception {
         MyBlockingQueue<Integer> queue = new MyBlockingQueue<>(5);
         List<Integer> tooLargeList = Arrays.asList(1, 2, 3, 4, 5, 6);
-        Thread testingThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                queue.offer(tooLargeList);
-            }
+        Thread testingThread = new Thread(() -> {
+            queue.offer(tooLargeList);
         });
         testingThread.start();
         Thread.sleep(50);
@@ -55,5 +55,53 @@ public class MyBlockingQueueTest {
     @Test(expected = IllegalArgumentException.class)
     public void testBadSize() {
         queue.take(-10);
+    }
+
+    @Test(timeout = 100)
+    public void testSingleThreadQueue() {
+        List<Integer> firstList = Arrays.asList(1, 2, 3);
+        List<Integer> secondList = Arrays.asList(6, 5);
+        queue.offer(firstList);
+        queue.offer(secondList);
+        List<Integer> answerList1 = queue.take(2);
+        List<Integer> answerList2 = queue.take(3);
+        assertThat(answerList1.size(), is(2));
+        assertThat(answerList1, contains(1, 2));
+        assertThat(answerList2.size(), is(3));
+        assertThat(answerList2, contains(3, 6, 5));
+    }
+
+    @Test(timeout = 1000)
+    public void testMultiThreadQueue() throws ExecutionException, InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        List<Integer> answerList1;
+        List<Integer> answerList2;
+        Future<List<Integer>> answer1 = executor.submit(() -> queue.take(2));
+        Thread.sleep(100);
+        Future<List<Integer>> answer2 = executor.submit(() -> queue.take(3));
+        List<Integer> firstList = Arrays.asList(1, 2, 3);
+        List<Integer> secondList = Arrays.asList(6, 5);
+        queue.offer(firstList);
+        queue.offer(secondList);
+        answerList1 = answer1.get();
+        answerList2 = answer2.get();
+        assertThat(answerList1.size(), is(2));
+        assertThat(answerList1, contains(1, 2));
+        assertThat(answerList2.size(), is(3));
+        assertThat(answerList2, contains(3, 6, 5));
+    }
+
+    @Test(timeout = 1000)
+    public void testMultiThreadOffering() throws Exception {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        List<Integer> answerList;
+        queue.offer(Arrays.asList(1, 2, 3, 4, 5));
+        executor.submit(() -> queue.offer(Arrays.asList(1, 2, 3)));
+        Thread.sleep(100);
+        executor.submit(() -> queue.offer(Arrays.asList(6, 5)));
+        answerList = queue.take(10);
+        assertThat(answerList.size(), is(10));
+        List<Integer> rightAnswer = Arrays.asList(1, 2, 3, 4, 5, 1, 2, 3, 6, 5);
+        assertThat(answerList, is(rightAnswer));
     }
 }
